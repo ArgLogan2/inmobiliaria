bf79427d6012b5607dc8e5f6a654e2995d63f1e7
Miguel Perez <miguellrvc@gmial.com>
2022-08-21 20:04:47 -0300

    se realiza la inyección en home page

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -9,8 +9,32 @@ import { Inmobiliaria } from '../../models/home';
 })
 export class HomePageComponent implements OnInit {
 
+  public inmobiliaria : Inmobiliaria[] = []; 
+  path: string = "../../../../../assets/img/";
+  imgList = ['LaFalda.jpg','VillaGiardino2.jpg','VillaGiardino1.jpg','LaFalda2.jpg','LaCumbre.jpg','LosCocos.jpg','CapillaDelMonte.jpg','CerroUritorco.jpg','CarlosPaz.jpg','cosquin1.jpg','cerro_la_cruz.jpg','vista-de-la-ciudad-de-villa-carlos-paz-y-lago-san-roque.jpg'];
+  index: number = 0;
+  image: string = "";
+  constructor(private homeService:DataLoadHomeService) { }
 
-  constructor() { }
+  ngOnInit(): void {
 
-  ngOnInit(): void {}
+    setInterval(() =>{
+      this.image = this.path + this.imgList[this.index];
+      // console.log(this.image); 
+      this.index++;
+      if (this.index == this.imgList.length){this.index =0;}
+    }, 5000);
+
+    this.getsInmobiliaria();
+
+  }
+
+  getsInmobiliaria(){
+    this.homeService.getsInmobiliaria()
+    .subscribe((inmobiliaria:Inmobiliaria[]) => {
+      this.inmobiliaria = inmobiliaria; 
+      console.log(this.inmobiliaria);
+    })
+  }
+
 }
